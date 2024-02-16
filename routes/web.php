28d0321f9420e11a277aa386742147f95f4d021e
Samuel Souza <85644942+samue345@users.noreply.github.com>
2024-02-16 19:07:04 -0300

    criando model e redirectController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RedirectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/r/{redirect}', [RedirectController::class, 'createRedirect'])->where('redirect', '.*');
